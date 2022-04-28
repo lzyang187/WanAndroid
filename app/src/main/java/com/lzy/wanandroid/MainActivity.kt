80e64545561d7f95d8adf7e94c57c438a68e37b1
zhaoyang.li5 <zhaoyang.li5@transsion.com>
2022-04-28 14:52:35 +0800

    feat:加入抽屉布局

--- app/src/main/java/com/lzy/wanandroid/MainActivity.kt
+++ app/src/main/java/com/lzy/wanandroid/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.lzy.wanandroid
 
+import android.view.MenuItem
+import androidx.core.view.GravityCompat
 import androidx.viewpager2.widget.ViewPager2
 import com.lzy.libview.BaseActivity
 import com.lzy.libview.ViewPager2FragmentStateAdapter
@@ -28,12 +30,13 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
 
     override fun initView() {
         super.initView()
+        initToolBar()
         mBinding.viewPager.isUserInputEnabled = false
         mBinding.viewPager.orientation = ViewPager2.ORIENTATION_HORIZONTAL
         mBinding.viewPager.adapter = ViewPager2FragmentStateAdapter(
             this, listOf(mHomeFragment, mDashboardFragment, mNotificationsFragment)
         )
-        mBinding.navView.setOnItemSelectedListener { item ->
+        mBinding.bottomNavView.setOnItemSelectedListener { item ->
             when (item.itemId) {
                 R.id.navigation_home -> {
                     mBinding.viewPager.currentItem = 0
@@ -47,5 +50,40 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
             }
             true
         }
+        // 抽屉布局
+        mBinding.navigationView.setNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.my_collect -> {
+                    toast(R.string.my_collect)
+                }
+                R.id.my_share -> {
+                    toast(R.string.my_share)
+                }
+            }
+            mBinding.drawerLayout.closeDrawers()
+            return@setNavigationItemSelectedListener true
+        }
     }
+
+    private fun initToolBar() {
+        setSupportActionBar(mBinding.toolbarLayout.toolbar)
+        mBinding.toolbarLayout.toolbar.setTitleTextAppearance(
+            this, com.lzy.libview.R.style.lib_view_toolbar_title
+        )
+        supportActionBar?.let {
+            it.setDisplayHomeAsUpEnabled(true)
+            it.setHomeAsUpIndicator(R.drawable.ic_baseline_menu_24)
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                mBinding.drawerLayout.openDrawer(GravityCompat.START)
+            }
+        }
+        return true
+    }
+
+
 }
